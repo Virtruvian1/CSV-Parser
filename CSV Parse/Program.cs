814c1685c27e4b35f5018bc373a69fa7ab38fd39
Zachary Allen <zachary.thomas.allen@outlook.com>
2020-03-12 10:44:16 -0400

    Add project files.

---
+++ CSV Parse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+using System.Diagnostics;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
